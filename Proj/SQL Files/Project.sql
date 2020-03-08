8a39a05a9d232039220b73acb9472549fb54a8b7
Niveth Saran <31360464+nivethsaran@users.noreply.github.com>
2020-03-08 20:24:05 +0530

    Update Project.sql

--- Proj/SQL Files/Project.sql
+++ Proj/SQL Files/Project.sql
@@ -68,4 +68,79 @@ select person.firstName,person.lastName,violation.violatorAadhar,count(violation
 
 
 
+
+#CREATE DATABASE
+create database traffic_monitoring_system;
+
+#SELECTING DATABASE
+use traffic_monitoring_system;
+
+#DROP COMMANDS
+drop table person;
+drop table officer;
+drop table license;
+drop table vehicle;
+drop table model_manufacturer;
+drop table accidents;
+drop table vehicle_accident;
+drop table vehicle_owner;
+drop table violation;
+drop table violation_penalty;
+
+
+
+#CREATE COMMANDS
+create Table Person(aadhar numeric(12), firstName varchar(30), lastName varchar(30), dob date, PINcode numeric(6), mobNumber numeric(10), email varchar(30), licenseID varchar(15), primary key(aadhar));
+create Table Model_Manufacturer(model varchar(25), manufacturer varchar(30), primary key (model));
+create Table Vehicle(LPN varchar(10), model varchar(25), insuranceCheck bool, PollutionCheck bool, primary key(LPN), foreign key(model) references Model_Manufacturer(model));
+create Table Officer(officerID varchar(20), passwordHash varchar(32), designation varchar(20), aadhar numeric(12), primary key(officerID), foreign key(aadhar) references Person(aadhar));
+create Table License(licenseID varchar(15), licenseIssueDate date, licenseExpiryData date, aadhar numeric(12), primary key(licenseID), foreign key(aadhar) references Person(aadhar));
+create Table Accidents(accidentID varchar(20), casualties numeric(2,0), timeOfAccident datetime, location varchar(30), cause varchar(30), officerInChargeID varchar(20), primary key(accidentID), foreign key(officerInChargeID) references Officer(officerID) );
+create Table Vehicle_Accident(accidentID varchar(20), LPN varchar(10), primary key(accidentID, LPN), foreign key (accidentID) references Accidents(accidentID), foreign key(LPN) references Vehicle(LPN));
+create Table Vehicle_Owner(LPN varchar(10), ownerAadhar numeric(12), primary key(LPN, ownerAadhar), foreign key(ownerAadhar) references Person(aadhar), foreign key(LPN) references Vehicle(LPN));
+create Table Violation_Penalty(type varchar(30), action varchar(30), fee numeric(6), primary key(type));
+create Table Violation(billID int(20), type varchar(30), officerInChargeID varchar(20), violatorAadhar numeric(12), primary key(billID), foreign key(officerInChargeID) references Officer(officerID), foreign key(type) references Violation_Penalty(type));
+ALTER TABLE Violation AUTO_INCREMENT=100;
+
+
+#VIEW ENTIRE TABLE
+select * from person;
+select * from officer;
+select * from license;
+select * from vehicle;
+select * from model_manufacturer;
+select * from accidents;
+select * from vehicle_accident;
+select * from vehicle_owner;
+select * from violation;
+select * from violation_penalty;person
+
+
+#DESCRIBE TABLES
+DESC person;
+DESC officer;
+DESC license;
+DESC vehicle;
+DESC model_manufacturer;
+DESC accidents;
+DESC vehicle_accident;
+DESC vehicle_owner;
+DESC violation;
+DESC violation_penalty;
+
+#delete from vehicle_owner where lpn!="";
+#delete from vehicle where lpn!="";
+#delete from vehicle_accident where lpn!="";
+#delete from accidents where accidentID!="";
+
+select count(*) from license;
+
+select person.firstname,person.lastname,person.mobNumber,T2.c from person,(select violatorAadhar as VA,count(Billid) as c from violation group by violatorAadhar) as T2 where person.aadhar=T2.VA order by T2.c desc;
+
+
+
+
+    
+
+
     
